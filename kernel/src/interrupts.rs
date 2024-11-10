2531412e596b2e413aba17d7c2d228853282bd46
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2024-11-10 11:38:57 -0800

    keyboard input

--- kernel/src/interrupts.rs
+++ kernel/src/interrupts.rs
@@ -1,3 +1,4 @@
+use pc_keyboard::KeyboardLayout;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode};
 
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
@@ -26,6 +27,39 @@ extern "x86-interrupt" fn general_protection_fault_handler(
     panic!("EXCEPTION: General Protection\n{:#?}", stack_frame);
 }
 
+extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStackFrame) {
+    use pc_keyboard::{layouts, DecodedKey, HandleControl, Keyboard, ScancodeSet1};
+    use spin::Mutex;
+    use x86_64::instructions::port::Port;
+
+    lazy_static! {
+        static ref KEYBOARD: Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>> =
+            Mutex::new(Keyboard::new(
+                ScancodeSet1::new(),
+                layouts::Us104Key,
+                HandleControl::Ignore
+            ));
+    }
+
+    let mut keyboard = KEYBOARD.lock();
+    let mut port = Port::new(0x60);
+
+    let scancode: u8 = unsafe { port.read() };
+    if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
+        if let Some(key) = keyboard.process_keyevent(key_event) {
+            match key {
+                DecodedKey::Unicode(character) => log::info!("{}", character),
+                DecodedKey::RawKey(key) => log::info!("{:?}", key),
+            }
+        }
+    }
+
+    unsafe {
+        PICS.lock()
+            .notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8());
+    }
+}
+
 use lazy_static::lazy_static;
 
 use crate::gtd::DOUBLE_FAULT_IST_INDEX;
@@ -43,6 +77,7 @@ lazy_static! {
         idt[InterruptIndex::Timer.as_u8()].set_handler_fn(timer_interrupt_handler);
         idt.general_protection_fault
             .set_handler_fn(general_protection_fault_handler);
+        idt[InterruptIndex::Keyboard.as_u8()].set_handler_fn(keyboard_interrupt_handler);
         idt
     };
 }
@@ -51,7 +86,7 @@ pub fn init_interrupts() {
     IDT.load();
     unsafe {
         let mut pics = PICS.lock();
-        pics.write_masks(0b11111110, 255);
+        pics.write_masks(0, 0);
         pics.initialize();
     };
     x86_64::instructions::interrupts::enable();
@@ -70,6 +105,7 @@ pub static PICS: spin::Mutex<ChainedPics> =
 #[repr(u8)]
 pub enum InterruptIndex {
     Timer = PIC_1_OFFSET,
+    Keyboard,
 }
 
 impl InterruptIndex {
