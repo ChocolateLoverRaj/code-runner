655c8fffaa9b4061427b2192df74397be14c52e1
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2025-03-20 15:12:04 -0700

    Fix limine script and gitignroe

--- build_limine.sh
+++ build_limine.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 LIMINE_PATH=$(NIX_PATH="nixpkgs=./nixpkgs" nix-build -E "with import <nixpkgs> { }; callPackage ./nixpkgs/pkgs/by-name/li/limine/package.nix { enableAll = true; }")
-rm ./limine
+rm ./limine || true
 ln -s $LIMINE_PATH ./limine
